0601f797f8850cdeeab127d23120b4369c430f31
vlopes <vlopes@Viniciuss-MacBook-Pro.local>
2025-07-25 00:13:36 -0400

    initial setup

---
+++ testcase-2-commits/src/test/java/testcases/CWE113_HTTP_Response_Splitting/s01/CWE113_HTTP_Response_Splitting__Environment_addCookieServlet_03Test.java
@@ -0,0 +1,43 @@
+package testcases.CWE113_HTTP_Response_Splitting.s01;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+
+public class CWE113_HTTP_Response_Splitting__Environment_addCookieServlet_03Test {
+
+    /**
+     * Test method to verify the presence of HTTP Response Splitting vulnerability.
+     * This test will pass if the vulnerability exists, i.e., if unsanitized input
+     * from an environment variable is added directly to a cookie.
+     */
+    @Test
+    public void testBad() throws Throwable {
+        // Mocking HttpServletRequest and HttpServletResponse
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+
+        // Setting up the environment variable to simulate malicious input
+        String maliciousInput = "en-US%0d%0aSet-Cookie:sessionId=malicious";
+        System.setProperty("ADD", maliciousInput);
+
+        // Creating an instance of the class to be tested
+        CWE113_HTTP_Response_Splitting__Environment_addCookieServlet_03 servlet =
+                new CWE113_HTTP_Response_Splitting__Environment_addCookieServlet_03();
+
+        // Invoking the 'bad' method which is expected to be vulnerable
+        servlet.bad(request, response);
+
+        // Verifying that the cookie was added with unsanitized input
+        verify(response, times(1)).addCookie(argThat(cookie -> {
+            // Check if the cookie value contains the malicious input
+            return cookie.getValue().equals(maliciousInput);
+        }));
+    }
+}
